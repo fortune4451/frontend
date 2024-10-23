a68268d7dbc5096324040449553bc00dd47b3ed3
Prime <fortunev36@gmail.com>
2024-10-23 15:17:00 +0100

    Reversed to previous commit

--- src/app/userdashboard/transaction/page.tsx
+++ src/app/userdashboard/transaction/page.tsx
@@ -137,7 +137,7 @@ export default function TransactionPage(): JSX.Element {
             accessorKey: 'createdAt',
             header: 'Date',
             cell: ({ row }) => {
-                const createdAt = row.getValue('createdAt') as Date
+                const createdAt = new Date(row.getValue('createdAt'))
                 const date = createdAt.toLocaleDateString()
                 const time = createdAt.toLocaleTimeString([], {
                     hour: '2-digit',
